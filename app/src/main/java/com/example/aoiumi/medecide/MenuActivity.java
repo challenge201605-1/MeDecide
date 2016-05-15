558ea3dddea8350e9892b9faa56b9621c8058239
aoiumi <mofumofu.mochipuyo.gyu@gmail.com>
2016-05-15 16:13:30 +0900

    画面を作った

--- app/src/main/java/com/example/aoiumi/medecide/MenuActivity.java
+++ app/src/main/java/com/example/aoiumi/medecide/MenuActivity.java
@@ -3,12 +3,13 @@ package com.example.aoiumi.medecide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Result extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        setContentView(R.layout.activity_menu);
+
 
     }
 }
--- app/src/main/java/com/example/aoiumi/medecide/ResultActivity.java
+++ app/src/main/java/com/example/aoiumi/medecide/ResultActivity.java
@@ -3,13 +3,12 @@ package com.example.aoiumi.medecide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Menu extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
+        setContentView(R.layout.activity_result);
 
     }
 }
